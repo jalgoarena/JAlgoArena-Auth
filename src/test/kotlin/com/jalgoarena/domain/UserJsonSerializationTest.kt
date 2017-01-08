4a27b24f49c07160b4e33a3c50101d9614c98c26
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-08 14:29:42 +0100

    - add unit test for serialization of User

---
+++ src/test/kotlin/com/jalgoarena/domain/UserJsonSerializationTest.kt
@@ -0,0 +1,53 @@
+package com.jalgoarena.domain
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.assertj.core.api.Assertions.assertThat
+import org.intellij.lang.annotations.Language
+import org.junit.Before
+import org.junit.Test
+import org.springframework.boot.test.json.JacksonTester
+
+class UserJsonSerializationTest {
+
+    private lateinit var json: JacksonTester<User>
+
+    @Before
+    fun setup() {
+        val objectMapper = jacksonObjectMapper()
+        JacksonTester.initFields(this, objectMapper)
+    }
+
+    @Test
+    fun should_serialize_user() {
+        assertThat(json.write(USER))
+                .isEqualToJson("user.json")
+    }
+
+    @Test
+    fun should_deserialize_user() {
+        assertThat(json.parse(USER_JSON))
+                .isEqualTo(USER)
+    }
+
+    private val USER = User(
+        username = "mikołaj",
+        password = "blabla",
+        email = "mikolaj@email.com",
+        region = "Kraków",
+        team = "Tyniec Team",
+        role = Role.USER,
+        id = "0-0"
+    )
+
+    @Language("JSON")
+    private val USER_JSON = """{
+  "username": "mikołaj",
+  "password": "blabla",
+  "email": "mikolaj@email.com",
+  "region": "Kraków",
+  "team": "Tyniec Team",
+  "role": "USER",
+  "id": "0-0"
+}
+"""
+}

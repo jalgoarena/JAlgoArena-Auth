811c6a2cace9a935aad46ace6039c5d45dad5698
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-13 12:30:06 +0100

    - add unit tests for RawAccessJwtTokenSpec (handle exceptions for bad token)

--- src/test/kotlin/com/jalgoarena/security/token/RawAccessJwtTokenSpec.kt
+++ src/test/kotlin/com/jalgoarena/security/token/RawAccessJwtTokenSpec.kt
@@ -6,6 +6,7 @@ import org.junit.Before
 import org.junit.Test
 import org.mockito.BDDMockito.given
 import org.mockito.Mockito.mock
+import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.authority.SimpleGrantedAuthority
 import org.springframework.security.core.userdetails.User
 
@@ -28,6 +29,17 @@ class RawAccessJwtTokenSpec {
     fun parses_authentication_token() {
         val claims = RawAccessJwtToken(token!!).parseClaims(DUMMY_TOKEN_SIGNING_KEY)
         assertThat(claims.body.subject).isEqualTo(USER.username)
+    }
+
+    @Test(expected = JwtExpiredTokenException::class)
+    fun throws_expired_jwt_token_exception_when_token_expires() {
+        given(jwtSettings.tokenExpirationTime).willReturn(0)
+        RawAccessJwtToken(token!!).parseClaims(DUMMY_TOKEN_SIGNING_KEY)
+    }
+
+    @Test(expected = BadCredentialsException::class)
+    fun throws_bad_credentials_exception_when_token_is_corrupted() {
+        RawAccessJwtToken("corrupted_token").parseClaims(DUMMY_TOKEN_SIGNING_KEY)
     }
 
     private val USER = User("julia", "", listOf(SimpleGrantedAuthority("ROLE_USER")))

cbc1f01ddc22bd5b77384fb5fadeefc09ce4b58c
Jacek Spólnik <jacek.spolnik@gmail.com>
2017-01-30 11:17:33 +0100

    - bug fixes - fix run.sh script

--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-EUREKA_URL=http://localhost:5000/eureka/
-PORT=9999
-nohup java -jar jalgoarena-auth-*.jar &
+EUREKA_URL=http://localhost:5000/eureka/ PORT=9999 nohup java -jar jalgoarena-auth-*.jar &

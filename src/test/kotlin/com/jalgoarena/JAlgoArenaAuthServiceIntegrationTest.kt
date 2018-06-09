783a774cb7656312633b7897163056c6533973c8
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-06-09 18:57:01 +0200

    - test fixes

--- src/test/kotlin/com/jalgoarena/JAlgoArenaAuthServiceIntegrationTest.kt
+++ src/test/kotlin/com/jalgoarena/JAlgoArenaAuthServiceIntegrationTest.kt
@@ -1,12 +1,9 @@
 package com.jalgoarena
 
-import com.jalgoarena.data.UsersRepository
-import com.netflix.discovery.EurekaClient
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.mock.mockito.MockBean
 import org.springframework.boot.test.web.client.TestRestTemplate
 import org.springframework.test.context.junit4.SpringRunner
 import javax.inject.Inject
@@ -16,12 +13,6 @@ import javax.inject.Inject
 class JAlgoArenaAuthServiceIntegrationTest {
     @Inject
     private lateinit var restTemplate: TestRestTemplate
-
-    @MockBean
-    private lateinit var usersRepository: UsersRepository
-
-    @MockBean
-    private lateinit var eurekaClient: EurekaClient
 
     @Test
     fun check_if_spring_configuration_works_properly() {

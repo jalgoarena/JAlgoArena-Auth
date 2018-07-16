4fc5c6eb15c6991a8ef6062bb0ab250f94e45fda
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-16 18:01:56 +0200

    - fix handling errors (email/username already used)

--- src/main/kotlin/com/jalgoarena/web/UsersController.kt
+++ src/main/kotlin/com/jalgoarena/web/UsersController.kt
@@ -14,38 +14,35 @@ import javax.inject.Inject
 @RestController
 class UsersController(@Inject private val repository: UsersRepository) {
 
-    @GetMapping("/users", produces = arrayOf("application/json"))
+    @GetMapping("/users", produces = ["application/json"])
     fun publicUsers() = repository.findAll().map {
         it.copy(password = "", email = "")
     }
 
-    @PutMapping("/api/users", produces = arrayOf("application/json"))
+    @PutMapping("/api/users", produces = ["application/json"])
     fun updateUser(@RequestBody user: User) =
             repository.update(user).copy(password = "")
 
-    @GetMapping("/api/users", produces = arrayOf("application/json"))
+    @GetMapping("/api/users", produces = ["application/json"])
     fun users() = repository.findAll().map { it.apply { password = "" } }
 
-    @GetMapping("/api/user", produces = arrayOf("application/json"))
+    @GetMapping("/api/user", produces = ["application/json"])
     fun user(principal: Principal) =
             repository.findByUsername(principal.name).apply { password = "" }
 
-    @PostMapping("/signup", produces = arrayOf("application/json"))
-    fun signup(@RequestBody user: User): ResponseEntity<*> = handleExceptions {
-        ResponseEntity(repository.add(user).apply { password = "" }, HttpStatus.CREATED)
-    }
+    @PostMapping("/signup", produces = ["application/json"])
+    fun signup(@RequestBody user: User) =
+            ResponseEntity(repository.add(user).apply { password = "" }, HttpStatus.CREATED)
 
-    private fun handleExceptions(body: () -> ResponseEntity<*>): ResponseEntity<*> = try {
-        body()
-    } catch(e: Throwable) {
-        @Language("JSON")
-        fun error(message: String) = ResponseEntity.status(HttpStatus.CONFLICT)
-                .body("{ \"error\": \"Registration Error\", \"message\": \"$message\" }")
+    @Language("JSON")
+    private fun error(message: String) = ResponseEntity.status(HttpStatus.CONFLICT)
+            .body("{ \"error\": \"Registration Error\", \"message\": \"$message\" }")
 
-        when (e) {
-            is EmailIsAlreadyUsedException -> error("Email is already used")
-            is UsernameIsAlreadyUsedException -> error("Username is already used")
-            else -> throw e
-        }
-    }
+    @ExceptionHandler
+    fun usernameIsAlreadyUsed(ex: UsernameIsAlreadyUsedException)
+            = error(ex.message!!)
+
+    @ExceptionHandler
+    fun emailIsAlreadyUsed(ex: EmailIsAlreadyUsedException)
+            = error(ex.message!!)
 }

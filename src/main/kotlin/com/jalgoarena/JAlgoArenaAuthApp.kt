668f8b9c6fae8628fc94d5635246c6fe8ebca728
Jacek Spólnik <jacek.spolnik@gmail.com>
2018-07-27 09:42:35 +0200

    - update diagram

--- src/main/kotlin/com/jalgoarena/JAlgoArenaAuthApp.kt
+++ src/main/kotlin/com/jalgoarena/JAlgoArenaAuthApp.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 
-
 @SpringBootApplication
 @EnableConfigurationProperties
 @Configuration
